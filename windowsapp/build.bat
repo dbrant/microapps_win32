50e9f9311959471fd7e09ea8571a67ab0e5f16e1
Dmitry Brant <me@dmitrybrant.com>
2022-12-07 20:03:12 -0500

    Add comment.

--- windowsapp/build.bat
+++ windowsapp/build.bat
@@ -1,5 +1,6 @@
 rem Run this script from the Visual Studio Developer Command Prompt.
 
+rem Compile the resources into a .res file, for consumption by the linker.
 rc winapp.rc
 
-cl.exe -nologo /D "NDEBUG" /D "NDEBUG" /D "_WINDOWS" /D "_UNICODE" /D "UNICODE" /GL /W3 /Zc:wchar_t /fp:fast /Gm- /O2 /GR- /EHa- /Oi /Ot /GS- /arch:AVX2 /Gd /Oi /MT /FC winapp.cpp winapp.res /link /nodefaultlib user32.lib kernel32.lib gdi32.lib
+cl.exe -nologo /D "NDEBUG" /D "_WINDOWS" /D "_UNICODE" /D "UNICODE" /GL /W3 /Zc:wchar_t /fp:fast /Gm- /O2 /GR- /EHa- /Oi /Ot /GS- /arch:AVX2 /Gd /Oi /MT /FC winapp.cpp winapp.res /link /nodefaultlib user32.lib kernel32.lib gdi32.lib
